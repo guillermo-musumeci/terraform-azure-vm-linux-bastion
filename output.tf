5802473ca349a57e1f54e14a6c65757084ea6dba
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-02-19 11:12:50 +0100

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-# ID of Linux VM
-output "bastion-linux-id" {
-   value = "${azurerm_virtual_machine.tf-bastion-linux-vm.id}"
-}
-
-# Public IP of Bastion-linux VM
-output "bastion-linux-public-ip" {
-   value = "${azurerm_public_ip.tf-bastion-linux-ip.ip_address}"
-}
